de8106dffeff81c897ff600609c519e4a76dfaa4
RoshiniVoriganti <roshini.voriganti@gmail.com>
2025-07-15 16:08:18 +0530

    Create lessthangreaterthan.java

---
+++ lessthangreaterthan.java
@@ -0,0 +1,14 @@
+class lessthangreaterthan{
+    public static void main(String[] args){
+        int a=5;
+        if (a==5){
+            System.out.println("it is 5");
+        }
+        else if (a<5){
+            System.out.println("it is less than 5");
+        }
+        else{
+            System.out.println("it is greater than 5");
+        }
+    }
+}
