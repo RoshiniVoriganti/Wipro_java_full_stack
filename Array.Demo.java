343baddd2295d5c1b99ea5dd04aee4fb95a76c0f
Roshini Voriganti <roshini.voriganti@gmail.com>
2025-07-22 20:29:22 +0530

    Create Array.Demo.java

---
+++ Array.Demo.java
@@ -0,0 +1,12 @@
+public class ArrayDemo{
+public static void main(String[]args){
+int[]x;
+x=new int[5];
+x[0]=07;
+x[1]=25;
+x[2]=38;
+System.out.println("Element at index 0:"+x[0]);
+System.out.println("Element at index 1:"+x[1]);
+System.out.println("Element at index 2:"+x[2]);
+}
+}
