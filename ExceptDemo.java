0a896366fb9849b028e78695f3249054a63c730d
Roshini Voriganti <roshini.voriganti@gmail.com>
2025-07-22 20:42:40 +0530

    Create ExceptDemo.java

---
+++ ExceptDemo.java
@@ -0,0 +1,18 @@
+package exceptiondemo;
+
+public class ExceptDemo {
+
+
+	public static void main(String[] args) {
+		int x,a;
+		try {
+			x=0;
+			a=22/x;
+			System.out.println("This will be bypassed.");
+		}
+		catch(ArithmeticException e) {
+			System.out.println("Division by zero.");
+		}
+		System.out.println("After catch statement");
+	}
+}
